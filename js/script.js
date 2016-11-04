1f71f33dae444fe2b63484188b8f4f842b64b0ea
Chin <MChin@gsmarketing.com>
2016-11-04 12:22:54 -0500

    colored rows

--- js/script.js
+++ js/script.js
@@ -5,18 +5,23 @@ $(document).ready(function () {
     // Hosted the example JSON on a free website
     $.getJSON("https://api.myjson.com/bins/3ofhy", function(json) {
         drawTable(json); 
+        // $("#row_1").css({'background-color': '#0101ff'});
     });
 
     
 
     function drawTable(data) {
+        
         for (var i = 0; i < data.info.length; i++) {
-            drawRow(data.info[i]);
+            drawRow(data.info[i],i);
         }
     }
 
-    function drawRow(rowData) {
-        var row = $("<tr />")
+    function drawRow(rowData, num) {
+        var gst_avg = 5.7;
+        var row = $("<tr id=\"row_" + num + "\">");
+        // row.append($(num + "\">"));
+
         $("#dataTable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
         row.append($("<td>" + rowData.ENTITY_NAME + "</td>"));
         row.append($("<td>" + rowData.RTL_TODAY_SLS + "</td>"));
@@ -38,5 +43,19 @@ $(document).ready(function () {
         row.append($("<td>" + rowData.TCUV_MTD_TODAY + "</td>"));
         row.append($("<td>" + rowData.TCUV_OBJECTIVE + "</td>"));
         row.append($("<td>" + rowData.TCUV_PER_OBJ + "</td>"));
+        row.append($("</tr>"));
+
+        if (rowData.RTL_PER_OBJ < gst_avg) {
+            $("#row_" + num).css({"background-color": "#c0fefe"});
+        }
+        
+
+
+
+
     }
+
+
 });
+    
+
